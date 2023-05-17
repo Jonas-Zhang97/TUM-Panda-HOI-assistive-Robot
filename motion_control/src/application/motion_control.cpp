0cd1e177f1c72d072e19bfc42700d336428ab84d
Yueyang Zhang <yueyang.zhang@tum.de>
2023-05-17 10:33:57 +0200

    modified the code structure and the comments

--- motion_control/src/application/motion_control.cpp
+++ motion_control/src/application/motion_control.cpp
@@ -1,38 +1,3 @@
-/*********************************************************************
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2013, SRI International
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of SRI International nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
-
-/* Author: Sachin Chitta, Dave Coleman, Mike Lautman */
 #include <moveit_msgs/DisplayRobotState.h>
 #include <moveit_msgs/DisplayTrajectory.h>
 
@@ -49,89 +14,52 @@
 #include <motion_control/robot.h>
 #include <motion_control/scene.h>
 
-// 1. The circle constant tau = 2*pi. One tau is one rotation in radians.
 const double tau = 2*M_PI;
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "move_group_interface_tutorial");
+  // Initialize the node
+  ros::init(argc, argv, "motion_control_node");
   ros::NodeHandle node_handle;
 
-  // ROS spinning must be running for the MoveGroupInterface to get information
-  // about the robot's state. One way to do this is to start an AsyncSpinner
-  // beforehand.
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  // BEGIN_TUTORIAL
-  //
-  // Setup
-  // ^^^^^
-
+  // Create objects to get access to my libraries
   RobotMotion robot_motion;
   RvizScene rviz_scene;
+
+  // Set parameters for Rviz
   namespace rvt = rviz_visual_tools;
 
   Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
   text_pose.translation().z() = 1.0;
-  //
-  // MoveIt operates on sets of joints called "planning groups" and stores them in an object called
-  // the `JointModelGroup`. Throughout MoveIt the terms "planning group" and "joint model group"
-  // are used interchangeably.
 
-  // Visualization
-  // ^^^^^^^^^^^^^
-  //
-  // The package MoveItVisualTools provides many capabilities for visualizing objects, robots,
-  // and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script.
-  rviz_scene.visual_tools.deleteAllMarkers();
-
-  rviz_scene.PSI.removeCollisionObjects(rviz_scene.PSI.getKnownObjectNames());
-
-  // Remote control is an introspection tool that allows users to step through a high level script
-  // via buttons and keyboard shortcuts in RViz
   rviz_scene.visual_tools.loadRemoteControl();
 
-  // RViz provides many types of markers, in this demo we will use text, cylinders, and spheres
+  // Initialize the scene in Rviz
+  rviz_scene.visual_tools.deleteAllMarkers();
   
-  rviz_scene.visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
+  rviz_scene.PSI.removeCollisionObjects(rviz_scene.PSI.getKnownObjectNames());
+  
+  rviz_scene.visual_tools.publishText(text_pose, "Motion Visualization", rvt::WHITE, rvt::XLARGE);
 
-  // Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations
   rviz_scene.visual_tools.trigger();
 
-  // Getting Basic Information
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
-  //
-  // We can print the name of the reference frame for this robot.
-
-  // Start the demo
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
-
+  // Reset Karina position
   robot_motion.homing();
+
   rviz_scene.visual_tools.prompt("Press 'next' to continue");
 
-//   std::vector<moveit_msgs::CollisionObject> objects = addCollisionObjects(planning_scene_interface);
-// 
-//   std::vector<geometry_msgs::PoseStamped> pick_poses = getPickPoses(objects, move_group_interface);
-// 
-//   pick(move_group_interface, move_group_interface_hand, pick_poses);
-// 
-//   control_msgs::GripperCommand gripper_command;
-// 
-//   visual_tools.prompt("The next part is just for test");
-
+  // Start motion
   robot_motion.hand_open();
   robot_motion.homing();
 
-  // robot_motion.hand_close(move_group_interface_hand);
-
-  // Wait for MoveGroup to receive and process the attached collision object message
   rviz_scene.visual_tools.prompt("Press 'next' to end the demo");
 
   rviz_scene.PSI.removeCollisionObjects(rviz_scene.PSI.getKnownObjectNames());
 
-  // END_TUTORIAL
-
+  // End the node
   ros::shutdown();
   return 0;
 }
