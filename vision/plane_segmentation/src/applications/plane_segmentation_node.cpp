3f5da3aae87a0e402bccce1e980b314ccfef67c1
Yueyang Zhang <yueyang.zhang@tum.de>
2023-06-15 15:09:06 +0200

    add object_labeling

--- vision/plane_segmentation/src/applications/plane_segmentation_node.cpp
+++ vision/plane_segmentation/src/applications/plane_segmentation_node.cpp
@@ -3,5 +3,18 @@
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "plane_segmentation");
+
+  PlaneSegmentation ps;
+
+  ps.init();
+
+  ros::Rate loop_rate(50);
+  while (ros::ok())
+  {
+    ps.update();
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
   return 0;
 }
